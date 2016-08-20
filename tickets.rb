704e229fe4712f8d21f6318140a094f60a88299b
Tyranja <tyranja@cassiopeia.uberspace.de>
2016-08-20 11:35:46 +0200

    Add price and attr_acce to Ticket class

--- tickets.rb
+++ tickets.rb
@@ -1,14 +1,27 @@
 class Ticket
+  attr_reader :venue, :date, :price
+  attr_writer :price
   def initialize(venue, date)
     @venue = venue
     @date = date
   end
 
-  def venue
-    @venue
+  def Ticket.most_expensive(*tickets)
+    tickets.max_by(&:price)
   end
+  #def venue
+    #@venue
+  #end
 
-  def date
-    @date
-  end
+  #def date
+    #@date
+  #end
+  #
+  #def price=(amount)
+  # @price = amount
+  #end
+  #
+  #def price
+  # @price
+  #end
 end

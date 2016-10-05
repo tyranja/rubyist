c017f8a588cd5ae47fdc8f405cbc543fac9f8d0a
tyranja <tyranja@cassiopeia.uberspace.de>
2016-10-05 16:43:57 +0200

    More files

---
+++ block.rb
@@ -0,0 +1,8 @@
+def block_variable
+   x = 13
+   puts x
+   3.times do |a|
+     puts x
+     puts a
+   end
+end
--- tickets.rb
+++ tickets.rb
@@ -11,4 +11,16 @@ class Ticket
   def date
     @date
   end
+
+  def price=(amount)
+    @price=amount
+  end
+
+  def price
+    @price
+  end
+
+  def laugh=(string)
+    puts "Hahaha"
+  end
 end
